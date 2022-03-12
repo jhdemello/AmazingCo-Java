c617465457f4f2e86f877cc9bc18ebfbe0ae0d7d
jhd.Mello <jhdemello@HP-Pavilion.localdomain>
2022-03-12 09:58:53 -0800

    [INIT] Awesome Re-Launch
         This is the initial transition of the Awesome Co. solution from a solely
         back-end solution to one that includes Dockerization, a GUI front-end
         (browser client), and data persistence via MySQL. This is a Springboot
         solution origninally lifted from:
    
              https://github.com/spring-projects/spring-petclinic.git
    
         This is the initial commit of the re-launch and is purely a fully
         functional, Dockerized implementation of the Springboot Pet Clinic example,
         sans any changes. Upon completion, if there  are any suspicious relics in
         the repository about pets, vets, and clinics, you now know why.

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-rm -f *.class *.jar
-
-javac Main.java AwesomeTreeNode.java
-jar cvf AwesomeCo.jar *.class
-jar cvfe AwesomeCo.jar Main *.class
-jar cvfm AwesomeCo.jar manifest.txt  *.class
-java -jar *.jar
