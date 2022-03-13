d581caacefaf4011e6605fa4f8ae230216fd3b1e
jhd.Mello <jhdemello@HP-Pavilion.localdomain>
2022-03-13 14:31:28 -0700

    [IMPL]: Added GET/POST Behaviors to Amazing Co
         Lifts existing GET/POST Veterinary list functionality and drops that
         functionality into the initial cut at creating the Amazing Co. employee
         directory (localhost:8080/employee_directory.html). The current incarnation
         displays a list of celebrity veterinarians. Needs more work.

--- src/main/java/org/springframework/samples/petclinic/amazingco/AmazingCoController.java
+++ src/main/java/org/springframework/samples/petclinic/amazingco/AmazingCoController.java
@@ -11,17 +11,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.samples.petclinic.vet.VetRepository;
+import org.springframework.samples.petclinic.vet.Vet;
+import org.springframework.samples.petclinic.vet.Vets;
+
 /**
  * @author Jason deMello
  */
 @Controller
-public class AmazingCoController {
+class AmazingCoController {
+
+	private final VetRepository vets;
+
+	public AmazingCoController(VetRepository clinicService) {
+		this.vets = clinicService;
+	}
+
+	@GetMapping("/employee_directory.html")
+	public String showVetList(@RequestParam(defaultValue = "1") int page, Model model) {
+		// Here we are returning an object of type 'Vets' rather than a collection of Vet
+		// objects so it is simpler for Object-Xml mapping
+		Vets vets = new Vets();
+		Page<Vet> paginated = findPaginated(page);
+		vets.getVetList().addAll(paginated.toList());
+		return addPaginationModel(page, paginated, model);
+	}
+
+	private String addPaginationModel(int page, Page<Vet> paginated, Model model) {
+		List<Vet> listVets = paginated.getContent();
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", paginated.getTotalPages());
+		model.addAttribute("totalItems", paginated.getTotalElements());
+		model.addAttribute("listVets", listVets);
+		return "amazingco/employeeList";
+	}
+
+	private Page<Vet> findPaginated(int page) {
+		int pageSize = 5;
+		Pageable pageable = PageRequest.of(page - 1, pageSize);
+		return vets.findAll(pageable);
+	}
 
 	@GetMapping({ "/amazingco" })
-	public String amazingco(@RequestParam(name = "name", required = false, defaultValue = "Mario") String name,
-			Model model) {
-		model.addAttribute("name", name);
-		return "amazingco";
+	public @ResponseBody Vets showResourcesVetList() {
+		// Here we are returning an object of type 'Vets' rather than a collection of Vet
+		// objects so it is simpler for JSon/Object mapping
+		Vets vets = new Vets();
+		vets.getVetList().addAll(this.vets.findAll());
+		return vets;
 	}
 
 }
