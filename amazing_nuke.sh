3cec4719849e55ca3f8a4ac89692ac5cad2167a5
jhd.Mello <jhdemello@HP-Pavilion.localdomain>
2022-03-18 15:00:27 -0700

    [README & TOOLS]:
         1)  Commits images created and linked to as issues in readme, and
         2)  Amazing Nuke
             a)  Stops and removes AmazingCo containers
    	 b)  Removes AmazingCo images
    	 c)  Removes AmazingCo volumes

---
+++ amazing_nuke.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+docker container stop amazingco-java_service_1 amazingco-java_mysqlserver_1
+docker container rm amazingco-java_service_1 amazingco-java_mysqlserver_1
+docker images | sort | grep none | awk '{ print $3 }' | xargs docker rmi
+docker rmi amazingco-java_service:latest mysql:8.0.23
+docker volume rm amazingco-java_mysql_config amazingco-java_mysql_data
